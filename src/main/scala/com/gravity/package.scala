24a4a796031cf576c5283ff0e0b793a13dffc999
equation <sivasaiemail@gmail.com>
2018-02-21 08:27:06 -0500

    updated

---
+++ src/main/scala/com/gravity/package.scala
@@ -0,0 +1,14 @@
+package com
+
+/**
+  * Created by UNIVERSE on 2/21/18.
+  */
+package object gravity {
+
+  case class ETable()
+  case class TCol( colName:String, udfToCall:String)
+
+   //= "column1"
+   //= "udfName"
+
+}
